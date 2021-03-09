b789ee812b7b51337ac714a6b244e9daff68fefe
Stu Bolderson <stu.bolderson@aol.com>
2021-03-09 07:56:47 +0100

    api request saved to CurrencyList.js

--- App/index.js
+++ App/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
 import { Home } from './screens/Home';
+import { CurrencyList } from './screens/CurrencyList';
 
-export default () => <Home />;
+export default () => <CurrencyList />;
---
+++ App/screens/CurrencyList.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { Text, View, FlatList } from 'react-native';
+
+export const CurrencyList = () => {
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.exchangeratesapi.io/latest')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(apiData);
+        setApiData([data.base, ...Object.keys(data.rates)]);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <FlatList
+      data={apiData}
+      renderItem={({ item }) => (
+        <View>
+          <Text>{item}</Text>
+        </View>
+      )}
+      keyExtractor={(item) => item}
+    />
+  );
+};
